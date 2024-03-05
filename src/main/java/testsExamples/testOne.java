6a44f07e53a62dc15d7c7fb7b08323c4124002ae
Rafael Dalmarco <111667173+rdalmarco@users.noreply.github.com>
2024-03-05 19:49:50 -0300

    Testes

--- src/main/java/testsExamples/testOne.java
+++ src/main/java/testsExamples/testOne.java
@@ -2,9 +2,37 @@ package testsExamples;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class testOne {
 
+    //Caso de teste
+    //Entrada - A (Arrange)
+    //Ação - A (Act)
+    //Saida - A (Assert)
     @Test
-    public void test() {
+    public void testOne() {
+        //Arrange
+        int a = 4;
+        int b = 2;
+
+        //Act
+        int res = a + b;
+
+        //Assert
+        assertEquals(6, res);
+    }
+
+    @Test
+    public void testTwo() {
+        //Arrange
+        int a = 4;
+        int b = 2;
+
+        //Act
+        int res = a * b;
+
+        //Assert
+        assertEquals(6, res);
     }
 }
